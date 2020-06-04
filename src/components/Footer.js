2530d1c6f6dd7779a9c2d46030b4a18260c395f9
hyperloo <hack.himanshu1024@gmail.com>
2020-06-04 11:48:47 +0530

    Footer Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,23 @@ import React from "react";
 const Footer = ({ paletteName, emoji }) => {
   return (
     <footer className="Palette-footer">
+      <span style={{ display: "inline-block", margin: "auto" }}>
+        Created by{" "}
+        <a
+          style={{
+            backgroundColor: "inherit",
+            textDecoration: "none",
+            cursor: "pointer",
+            zIndex: "100",
+            position: "relative",
+          }}
+          href="https://tekhin3.netlify.app"
+          target="_blank"
+        >
+          tekhin
+        </a>
+        , @ 2020
+      </span>
       {paletteName}
       <span className="emoji">{emoji}</span>
     </footer>
